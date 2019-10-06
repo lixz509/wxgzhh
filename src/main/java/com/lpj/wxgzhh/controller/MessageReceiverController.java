1f7a35bc96ce3e5713712c33d67949ccf2f45fbb
lixz509 <1979551686@qq.com>
2019-10-06 23:14:11 +0800

    设置验证

--- src/main/java/com/lpj/wxgzhh/controller/MessageReceiverController.java
+++ src/main/java/com/lpj/wxgzhh/controller/MessageReceiverController.java
@@ -1,7 +1,5 @@
 package com.lpj.wxgzhh.controller;
 
-import com.lpj.wxgzhh.service.WxgzhhService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/wxgzhh/message/receiver")
 public class MessageReceiverController {
 
-    @Autowired
-    private WxgzhhService wxgzhhService;
-
     @GetMapping
     public String echo(
             String signature,
@@ -20,13 +15,7 @@ public class MessageReceiverController {
             String nonce,
             String echostr
     ){
-        if(wxgzhhService.chexk(signature,timestamp,nonce)){
-            //成功则返回微信要求的参数
-            return echostr;
-        }else {
-            //失败则error
-            return echostr;
-        }
+        return echostr;
     }
 
 }
