8bcb9cff62b2061d48a08853b9bb3c734048a2b6
lixz509 <1979551686@qq.com>
2019-10-06 14:17:04 +0800

    创建项目

---
+++ src/test/java/com/lpj/wxgzhh/微信公众号ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lpj.wxgzhh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class 微信公众号ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
