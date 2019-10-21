9c18f9f32a2213aeaa940ca35e8cc110bab5b9b0
lixz509 <1979551686@qq.com>
2019-10-21 20:33:59 +0800

    消息处理

--- src/main/java/com/lpj/wxgzhh/controller/MessageReceiverController.java
+++ src/main/java/com/lpj/wxgzhh/controller/MessageReceiverController.java
@@ -1,21 +1,35 @@
 package com.lpj.wxgzhh.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/wxgzhh/message/receiver")
 public class MessageReceiverController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
+
     @GetMapping
     public String echo(
-            String signature,
-            String timestamp,
-            String nonce,
-            String echostr
+            @RequestParam("signature") String signature,
+            @RequestParam("timestamp") String timestamp,
+            @RequestParam("nonce") String nonce,
+            @RequestParam("echostr") String echostr
     ){
         return echostr;
     }
+
+    @PostMapping
+    public String onMessage(
+            @RequestParam("signature") String signature,
+            @RequestParam("timestamp") String timestamp,
+            @RequestParam("nonce") String nonce,
+            @RequestBody String xml){
+        LOG.trace("收到的消息原文：\n{}\n",xml);
+        return "success";
+    }
+
 
 }
