bce80a37a0bbc07597da7b46412e4acd90dfb048
lixz509 <1979551686@qq.com>
2019-10-28 22:35:54 +0800

    微信回复测试

--- src/main/java/com/lpj/wxgzhh/controller/MessageConvertController.java
+++ src/main/java/com/lpj/wxgzhh/controller/MessageConvertController.java
@@ -1,7 +1,9 @@
 package com.lpj.wxgzhh.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lpj.wxgzhh.domain.InMessage;
+import com.lpj.wxgzhh.domain.OutMessage;
 import com.lpj.wxgzhh.service.MessageAnalysisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +17,20 @@ import javax.xml.bind.JAXBException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.Charset;
+
 
 @RestController
 @RequestMapping("/wxgzhh/message/receiver")
@@ -28,6 +43,10 @@ public class MessageConvertController {
 
     @Autowired
     private RedisTemplate<String,InMessage> inMessageTemplate;
+
+    //测试
+    ObjectMapper objectMapper = new ObjectMapper();
+    private HttpClient httpClient = HttpClient.newBuilder().build();
 
     @GetMapping
     public String echo(
@@ -78,7 +97,25 @@ public class MessageConvertController {
                 "  <Content><![CDATA[你好]]></Content>\n" +
                 "</xml>";
 
-        return hf;
+
+        //测试回复消息
+
+        String openId=inMessage.getFromUserName();
+
+        String url="https://api.weixin.qq.com/customservice/kfaccount/add?access_token=lpj";
+
+        String text="655";
+
+        OutMessage out = new OutMessage(openId, text);
+
+        String json = this.objectMapper.writeValueAsString(out);
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url))//
+                // 以POST方式发送请求
+                .POST(BodyPublishers.ofString(json, StandardCharsets.UTF_8))//
+                .build();
+
+        return "success";
     }
 
 }
