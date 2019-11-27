f9331ad6e0b4446c87d8060eb594ff71660dd236
lixz509 <1979551686@>
2019-11-27 19:08:29 +0800

    将序列化程序放到更正确的位置

--- weixin/src/main/java/com/lpj/wxgzhh/service/OutMessageService.java
+++ weixin/src/main/java/com/lpj/wxgzhh/service/OutMessageService.java
@@ -6,7 +6,10 @@ import com.lpj.wxgzhh.domain.TextOutMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -17,8 +20,18 @@ public class OutMessageService {
 
     private static final Logger LOG = LoggerFactory.getLogger(OutMessageService.class);
 
-    @Autowired
-    private RedisTemplate<String,InMessage> inMessageTemplate;
+    //序列化和反序列化程序，用于将对象丢如redis消息队列
+    @Bean
+    public RedisTemplate<String, InMessage> inMessageTemplate(
+            @Autowired RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String,InMessage> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // 使用序列化程序完成对象的序列化和反序列化，可以自定义
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(InMessage.class));
+        return template;
+    }
 
     public String getRepose(InMessage inMessage) {
 
